8a0894be20ca920eaa46b1f6f4bad69dfdf9b9b9
Abdullah Al Mamun <abdullahmamun1597@gmail.com>
2022-05-01 09:51:54 +0600

    index.js add

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors())
+
+app.get('/', (req, res) => {
+  res.send("hello mamun, whats up..............");
+})
+
+app.listen(port, () => {
+  console.log("Listening to port", port);
+})
