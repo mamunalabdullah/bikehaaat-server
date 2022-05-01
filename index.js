9faa73ee07d1ebb29e2c28ce218e09d78cf67b82
Abdullah Al Mamun <abdullahmamun1597@gmail.com>
2022-05-01 21:36:29 +0600

    find and findone function add

--- index.js
+++ index.js
@@ -1,12 +1,53 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors())
+// middleware ////////////////////////////////
+
+app.use(cors());
+app.use(express.json());
+
+//////////////////////////////////////////////
+
+// MongoDb ///////////////////////////////////////////
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.cqkv4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+      await client.connect();
+      const inventoryCollection = client.db("bikeHaaat").collection("inventory");
+
+      app.get('/inventory', async(req, res) => {
+        const query = {};
+        const cursor = inventoryCollection.find(query);
+        const inventories = await cursor.toArray();
+        res.send(inventories);
+      })
+
+      app.get('/inventory/:id', async(req, res) => {
+        const id = req.params.id;
+        const query = {_id: ObjectId(id)};
+        const inventory = await inventoryCollection.findOne(query);
+        res.send(inventory);
+      })
+       
+    }
+    finally {
+    //   await client.close();
+    }
+  }
+  run().catch(console.dir);
+
+
+//////////////////////////////////////////////////////
 
 app.get('/', (req, res) => {
-  res.send("hello mamun, whats up..............");
+  res.send("server is running..............");
 })
 
 app.listen(port, () => {
